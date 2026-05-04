656aa42e88b951737243c0ab14c9f5adf9544497
Apzhyn <git@apzhyn.xyz>
2026-05-04 20:10:46 +0200

    rename my package in code for pypi

--- src/lib.rs
+++ src/lib.rs
@@ -67,7 +67,7 @@ fn run_sedu(input: String) -> Vec<String> {
 }
 
 #[pymodule]
-fn sedu(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn sedu_parse(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(run_sedu, m)?)?;
     Ok(())
 }
